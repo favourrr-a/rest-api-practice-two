d54931de5d2cd1be183351db7c35f0d7371fd379
nerdfavour <favourackonu13@gmail.com>
2023-12-18 04:24:45 +0000

    added route to about

---
+++ controllers/aboutController.js
@@ -0,0 +1,9 @@
+// controllers/aboutController.js
+const getAboutPage = (req, res) => {
+    res.send('About Us Page');
+  };
+  
+  module.exports = {
+    getAboutPage,
+  };
+  
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
+const aboutController = require('./controllers/aboutController');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (homeController.getHomePage))
+
+app.get('/about', (aboutController.getAboutPage))
 
 app.get('/blog', (req, res) => {
     res.send('whats good yall')
